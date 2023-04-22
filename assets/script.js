4db94924d9689a7342ecf3e733a6ac0ce7450b65
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-22 13:04:58 -0400

    adding html for questions and starting js to navigate questions

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,22 @@
 //timer to start 
 
 var countdown = getElementByID("countdown");
-const d = new Date();
-let time = d.getTime();
-console.log(now)
+
+let question1 = document.getElementByID("q1");
+let question2 = document.getElementByID("q2");
+let question3 = document.getElementByID("q3");
+let question4 = document.getElementByID("q4");
+let question5 = document.getElementByID("q5");
+
+
+//need function to get questions to progress to next after answered
+
+nextQuestion.onclick = function() {
+    if (nextQuestion > question1) {
+        question1.style.display = 'none'
+        question2.style.display = 'block' //how does this work with 5 questions?
+    }
+}
 
 //want to add event listener to the function
 //do I still need an Onclick on the html even though I have the click function on the start id here? 
