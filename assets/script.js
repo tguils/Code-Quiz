e3237bcfd59cab9b78a6d5886dba7232c4512991
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-23 11:52:02 -0400

    screen one completed

--- assets/script.js
+++ assets/script.js
@@ -1,95 +1,92 @@
-//timer to start 
+var startQuizButton = document.getElementById('startButton');
+var highScoreButton = document.querySelector('.highScoresButton');
+var remainingTime = document.getElementById('remainingTime');
 
-var countdown = getElementByID("countdown");
-//getting questions from text
-let question1 = document.getElementByID("q1");
-let question2 = document.getElementByID("q2");
-let question3 = document.getElementByID("q3");
-let question4 = document.getElementByID("q4");
+var screenOneWrapper = document.querySelector('.screen-1');
+var screenTwoWrapper = document.querySelector('.screen-2');
+var screenThreeWrapper = document.querySelector('.screen-3');
+var screenFourWrapper = document.querySelector('.screen-4');
 
-//getting options from text
-let option1 = document.getElementById("o1");
-let option1 = document.getElementById("o2");
-let option1 = document.getElementById("o3");
-let option1 = document.getElementById("o4");
+// Created variables
+// Screen 1 functions
 
-//I will set up the questions here
+function startQuiz() {
+    var timeLeft = 90;
 
-// var questions = [{
-//     id: 0,
-//     question: "Which of the following are JavaScript Dataypes?",
-//     answer: [{text: "boolean", isCorrect: false },
-//              { text: "quote", isCorrect: false },
-//              { text: "string", isCorrect: false },
-//              { text: "a and b", isCorrect: true }
-//         ]
-// },
+    setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval();
+        }
+        remainingTime.value = timeLeft;
+        remainingTime.textContent = timeLeft;
+        timeLeft -= 1;
+    },
+    1000);
+    
+    // hide the screen 1 and show screen 2
+    screenOneWrapper.classList.add("hidden");
+    screenTwoWrapper.classList.remove("hidden"); 
+}
 
-// {
-//     id: 1,
-//     question: "Which of the following are JavaScript Dataypes",
-//     answer: [{text: "boolean", isCorrect: false },
-//              { text: "quote", isCorrect: false },
-//              { text: "string", isCorrect: false },
-//              { text: "a and b", isCorrect: true }
-//         ]
-// },
 
-// {
-//     id: 2,
-//     question: "Which of the following are JavaScript Dataypes",
-//     answer: [{text: "boolean", isCorrect: false },
-//              { text: "quote", isCorrect: false },
-//              { text: "string", isCorrect: false },
-//              { text: "a and b", isCorrect: true }
-//         ]
-// },
+// Getting options from text
+    // let option1 = document.getElementById("o1");
+    // let option1 = document.getElementById("o2");
+    // let option1 = document.getElementById("o3");
+    // let option1 = document.getElementById("o4");
 
-// {
-//     id: 3,
-//     question: "Which of the following are JavaScript Dataypes",
-//     answer: [{text: "boolean", isCorrect: false },
-//              { text: "quote", isCorrect: false },
-//              { text: "string", isCorrect: false },
-//              { text: "a and b", isCorrect: true }
-//         ]
-//     }
-// ]
-// //need function to get questions to progress to next after answered
+// I will set up the questions here
 
-// nextQuestion.onclick = function() {
-//     if (nextQuestion > question1) {
-//         question1.style.display = 'none'
-//         question2.style.display = 'block' //how does this work with 5 questions?
-//     }
-// }
+    // var questions = [{
+    //     id: 0,
+    //     question: "Which of the following are JavaScript Dataypes?",
+    //     answer: [{text: "boolean", isCorrect: false },
+    //              { text: "quote", isCorrect: false },
+    //              { text: "string", isCorrect: false },
+    //              { text: "a and b", isCorrect: true }
+    //         ]
+    // },
 
-// //want to add event listener to the function
-// //do I still need an Onclick on the html even though I have the click function on the start id here? 
-// /*document.getElementById("start").addEventListener("click", function() {
-//     var timeLeft = 60;
-//     var runningTimer = setInterval(function() {
-//     document.getElementByID("countdown").innerHTML = timeLeft + "&nbsp" + "seconds remaining";
+    // {
+    //     id: 1,
+    //     question: "Which of the following are JavaScript Dataypes",
+    //     answer: [{text: "boolean", isCorrect: false },
+    //              { text: "quote", isCorrect: false },
+    //              { text: "string", isCorrect: false },
+    //              { text: "a and b", isCorrect: true }
+    //         ]
+    // },
 
-//     timeLeft -=1;
-//     if(timeLeft <= 0) {
-//         clearInterval(downloadTimer)
-//         document.getElementById("countdown").innerHTML = ("Time is up!")
-//     }
-// }, 1000);
-//         console.log(countdown);
-// }); */
-   
+    // {
+    //     id: 2,
+    //     question: "Which of the following are JavaScript Dataypes",
+    //     answer: [{text: "boolean", isCorrect: false },
+    //              { text: "quote", isCorrect: false },
+    //              { text: "string", isCorrect: false },
+    //              { text: "a and b", isCorrect: true }
+    //         ]
+    // },
 
-// //or different method 
-// var remainingTimeEl = document.querySelector("#remainingTime"),
-//     timeLeft = 60
+    // {
+    //     id: 3,
+    //     question: "Which of the following are JavaScript Dataypes",
+    //     answer: [{text: "boolean", isCorrect: false },
+    //              { text: "quote", isCorrect: false },
+    //              { text: "string", isCorrect: false },
+    //              { text: "a and b", isCorrect: true }
+    //         ]
+    //     }
+    // ]
 
-// var countdown = setInterval(function() {
-// if (timeLeft <= 0) clearInterval(downloadTimer) {
-//     remainingTimeEl.value = timeLeft;
-//     remainingTimeEl.textContent = timeLeft;
-//     timeLeft -= 1
-//     }
-// },
-// 1000)
+// Need function to get questions to progress to next after answered
+
+    // nextQuestion.onclick = function() {
+    //     if (nextQuestion > question1) {
+    //         question1.style.display = 'none'
+    //         question2.style.display = 'block' //how does this work with 5 questions?
+    //     }
+    // }
+
+// Screen Three work
+
+// Screen four final step
