a340922b91a68dd61ae6201fe461ed76cfb41897
LAPTOP-ELH40JFH\tessg <tess.guilmette@gmail.com>
2023-04-23 11:00:34 -0400

    Added CSS positioning, organizing code

--- assets/script.js
+++ assets/script.js
@@ -15,81 +15,81 @@ let option1 = document.getElementById("o4");
 
 //I will set up the questions here
 
-var questions = [{
-    id: 0,
-    question: "Which of the following are JavaScript Dataypes?",
-    answer: [{text: "boolean", isCorrect: false },
-             { text: "quote", isCorrect: false },
-             { text: "string", isCorrect: false },
-             { text: "a and b", isCorrect: true }
-        ]
-},
+// var questions = [{
+//     id: 0,
+//     question: "Which of the following are JavaScript Dataypes?",
+//     answer: [{text: "boolean", isCorrect: false },
+//              { text: "quote", isCorrect: false },
+//              { text: "string", isCorrect: false },
+//              { text: "a and b", isCorrect: true }
+//         ]
+// },
 
-{
-    id: 1,
-    question: "Which of the following are JavaScript Dataypes",
-    answer: [{text: "boolean", isCorrect: false },
-             { text: "quote", isCorrect: false },
-             { text: "string", isCorrect: false },
-             { text: "a and b", isCorrect: true }
-        ]
-},
+// {
+//     id: 1,
+//     question: "Which of the following are JavaScript Dataypes",
+//     answer: [{text: "boolean", isCorrect: false },
+//              { text: "quote", isCorrect: false },
+//              { text: "string", isCorrect: false },
+//              { text: "a and b", isCorrect: true }
+//         ]
+// },
 
-{
-    id: 2,
-    question: "Which of the following are JavaScript Dataypes",
-    answer: [{text: "boolean", isCorrect: false },
-             { text: "quote", isCorrect: false },
-             { text: "string", isCorrect: false },
-             { text: "a and b", isCorrect: true }
-        ]
-},
+// {
+//     id: 2,
+//     question: "Which of the following are JavaScript Dataypes",
+//     answer: [{text: "boolean", isCorrect: false },
+//              { text: "quote", isCorrect: false },
+//              { text: "string", isCorrect: false },
+//              { text: "a and b", isCorrect: true }
+//         ]
+// },
 
-{
-    id: 3,
-    question: "Which of the following are JavaScript Dataypes",
-    answer: [{text: "boolean", isCorrect: false },
-             { text: "quote", isCorrect: false },
-             { text: "string", isCorrect: false },
-             { text: "a and b", isCorrect: true }
-        ]
-    }
-]
-//need function to get questions to progress to next after answered
+// {
+//     id: 3,
+//     question: "Which of the following are JavaScript Dataypes",
+//     answer: [{text: "boolean", isCorrect: false },
+//              { text: "quote", isCorrect: false },
+//              { text: "string", isCorrect: false },
+//              { text: "a and b", isCorrect: true }
+//         ]
+//     }
+// ]
+// //need function to get questions to progress to next after answered
 
-nextQuestion.onclick = function() {
-    if (nextQuestion > question1) {
-        question1.style.display = 'none'
-        question2.style.display = 'block' //how does this work with 5 questions?
-    }
-}
+// nextQuestion.onclick = function() {
+//     if (nextQuestion > question1) {
+//         question1.style.display = 'none'
+//         question2.style.display = 'block' //how does this work with 5 questions?
+//     }
+// }
 
-//want to add event listener to the function
-//do I still need an Onclick on the html even though I have the click function on the start id here? 
-/*document.getElementById("start").addEventListener("click", function() {
-    var timeLeft = 60;
-    var runningTimer = setInterval(function() {
-    document.getElementByID("countdown").innerHTML = timeLeft + "&nbsp" + "seconds remaining";
+// //want to add event listener to the function
+// //do I still need an Onclick on the html even though I have the click function on the start id here? 
+// /*document.getElementById("start").addEventListener("click", function() {
+//     var timeLeft = 60;
+//     var runningTimer = setInterval(function() {
+//     document.getElementByID("countdown").innerHTML = timeLeft + "&nbsp" + "seconds remaining";
 
-    timeLeft -=1;
-    if(timeLeft <= 0) {
-        clearInterval(downloadTimer)
-        document.getElementById("countdown").innerHTML = ("Time is up!")
-    }
-}, 1000);
-        console.log(countdown);
-}); */
+//     timeLeft -=1;
+//     if(timeLeft <= 0) {
+//         clearInterval(downloadTimer)
+//         document.getElementById("countdown").innerHTML = ("Time is up!")
+//     }
+// }, 1000);
+//         console.log(countdown);
+// }); */
    
 
-//or different method 
-var remainingTimeEl = document.querySelector("#remainingTime"),
-    timeLeft = 60
+// //or different method 
+// var remainingTimeEl = document.querySelector("#remainingTime"),
+//     timeLeft = 60
 
-var countdown = setInterval(function() {
-if (timeLeft <= 0) clearInterval(downloadTimer) {
-    remainingTimeEl.value = timeLeft;
-    remainingTimeEl.textContent = timeLeft;
-    timeLeft -= 1
-    }
-},
-1000)
+// var countdown = setInterval(function() {
+// if (timeLeft <= 0) clearInterval(downloadTimer) {
+//     remainingTimeEl.value = timeLeft;
+//     remainingTimeEl.textContent = timeLeft;
+//     timeLeft -= 1
+//     }
+// },
+// 1000)
